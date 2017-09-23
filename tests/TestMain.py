0d1ce520583cdd8fd3f6111aa57f8484c418c0c9
Alexey <edgycircle@yandex.ru>
2017-09-24 00:43:05 +0300

    fix test

--- tests/TestMain.py
+++ tests/TestMain.py
@@ -1,6 +1,5 @@
 from DataUtils import *
-for key, value in get_category_comments_data(2050101):
-    print(str(key) + ": [")
-    for v in value.CATEGORY_ID.unique():
-        print(str(v) + ",")
+for key, value in get_category_comments_data(2050101).groupby("CATEGORY_NAME"):
+    for v in value.CATEGORY_NAME.unique():
+        print(str(v))
     print("]")
